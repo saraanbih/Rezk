b7c3a8841866e3eb4f789276dec66bbbf9d8d981
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2025-08-31 23:28:05 +0300

    responsive-updates2

--- Frontend/src/components/HowItWorks.jsx
+++ Frontend/src/components/HowItWorks.jsx
@@ -3,33 +3,35 @@ import arrow from "../assets/arrow.png";
 import clipBoard from "../assets/clipboard-export.png";
 import search from "../assets/search-normal.png";
 import userProfile from "../assets/user-tick.png";
+
+
+
 const HowItWorks = () => {
     return (
-        <div className = "my-[85px] ">
+        <div id='howItWorks' className = "my-[85px] ">
             <h2 className = "text-center font-medium text-[40px] text-[#2D9CDB] mb-[100px]">كييف تعمل المنصه؟</h2>
-             <div className = "flex items-center justify-center">
-                 <div className = "flex items-center justify-center w-full">
-                     <div className = "rounded-[20px] border border-[#A6A6A6] w-[350px] h-[310px] flex items-center justify-center flex-col gap-[10px]">
-                         <img src = {clipBoard}/>
-                         <h3 className = "font-bold !text-[32px]">قدم عليها</h3>
-                     </div>
-                 </div>
-                     <img src = {arrow} />
-                 <div className = "flex items-center justify-center w-full">
-                     <div className = "rounded-[20px] border border-[#A6A6A6] w-[350px] h-[310px] flex items-center justify-center flex-col gap-[10px]">
-                         <img src = {search}/>
-                         <h3 className = "font-bold !text-[32px] text-center w-[250px]">شوف الوظيفه المناسبه ليك</h3>
-                     </div>
-                 </div>
-                     <img src = {arrow} />
-                 <div className = "flex items-center justify-center w-full">
-                     <div className = "rounded-[20px] border border-[#A6A6A6] w-[350px] h-[310px] flex items-center justify-center flex-col gap-[10px]">
-                         <img src = {userProfile}/>
-                         <h3 className = "font-bold !text-[32px]">سجل دخولك </h3>
-                     </div>
-
-                 </div>
-             </div>
+                <div style={{display:"flex", alignItems:"center", flexWrap:"wrap", justifyContent:"center", gap:"30px"}}>
+                    <div className = "flex items-center justify-center ">
+                        <div className = "rounded-[20px] border border-[#A6A6A6] w-[350px] h-[310px] flex items-center justify-center flex-col gap-[10px]">
+                            <img src = {clipBoard}/>
+                            <h3 className = "font-bold !text-[32px]">قدم عليها</h3>
+                        </div>
+                    </div>
+                        <img src = {arrow} />
+                    <div className = "flex items-center justify-center ">
+                        <div className = "rounded-[20px] border border-[#A6A6A6] w-[350px] h-[310px] flex items-center justify-center flex-col gap-[10px]">
+                            <img src = {search}/>
+                            <h3 className = "font-bold !text-[32px] text-center w-[250px]">شوف الوظيفه المناسبه ليك</h3>
+                        </div>
+                    </div>
+                        <img src = {arrow} />
+                    <div className = "flex items-center justify-center ">
+                        <div className = "rounded-[20px] border border-[#A6A6A6] w-[350px] h-[310px] flex items-center justify-center flex-col gap-[10px]">
+                            <img src = {userProfile}/>
+                            <h3 className = "font-bold !text-[32px]">سجل دخولك </h3>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
